f394bb926122a475c92b9dfee44977d685f7933e
Dmitri Pisarenko <dp@altruix.co>
2013-09-22 02:02:07 +0400

    Added a static method for creating InternetAvailabilityChecker instances.

--- src/main/java/ru/altruix/commons/android/InternetAvailabilityChecker.java
+++ src/main/java/ru/altruix/commons/android/InternetAvailabilityChecker.java
@@ -3,6 +3,9 @@
  */
 package ru.altruix.commons.android;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 /**
@@ -17,6 +20,18 @@ public class InternetAvailabilityChecker implements
 	{
 		connectivityManager = aConnectivityManager;
 	}
+	
+	public static IInternetAvailabilityChecker create(final Activity aActivity) {
+		final ConnectivityManager connectivityManager = (ConnectivityManager) 
+				aActivity.getSystemService(Context.CONNECTIVITY_SERVICE);
+		final IConnectivityManager connectivityManagerWrapper = 
+				new ConnectivityManagerWrapper(connectivityManager);
+		
+		final IInternetAvailabilityChecker internetAvailabilityChecker = 
+				new InternetAvailabilityChecker(connectivityManagerWrapper);
+		return internetAvailabilityChecker;
+	}
+
 	
 	@Override
 	public boolean isInternetAvailable() {
